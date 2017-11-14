33fd665af2c9d0a949f21dc869a2fc1d7377c741
Drew Kitch <drew.kitch@gmail.com>
2017-11-14 15:12:42 -0700

    wired up for jet steps

--- app/config/test.exs
+++ app/config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
   database: "hello_test",
-  hostname: "localhost",
+  hostname: System.get_env("PGHOST"),
   pool: Ecto.Adapters.SQL.Sandbox
